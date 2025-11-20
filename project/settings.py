8fac6c30682dcb75ac30d354e17ce9048bd1be51
lazycoderhaha <lucylauren6550@gmail.com>
2025-11-20 18:53:50 +0500

    shda

--- project/settings.py
+++ project/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
