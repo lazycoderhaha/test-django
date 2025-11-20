dce4a2ef5422efa7bcb13cdc7cfeae768df3fa77
lazycoderhaha <lucylauren6550@gmail.com>
2025-11-20 19:44:20 +0500

    updated wsgi and created users app

--- project/wsgi.py
+++ project/wsgi.py
@@ -10,12 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.auth.models import User
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
-
-users = User.objects.all()
-if not users:
-    User.objects.create_superuser(username="admin", email="admin@gmail.com", password="testing123@", is_active=True, is_staff=True)
